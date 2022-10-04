770b6b6f6cb0448c9b3940e8bf6ec8d3aac86661
Dipendrakumar1 <dipendrayadav299@gmail.com>
2022-10-05 05:02:45 +0530

    added call-by-value and call-by-address concepts

--- Arrays_And_Pointers/02_pointer_basic.c
+++ Arrays_And_Pointers/02_pointer_basic.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+void printValue0(int p)
+{
+    // p=90;
+    printf("%d\n", p);
+}
+void printValue(int *p)
+{
+    *p = 90;
+    printf("%d\n", *p);
+}
 int main()
 {
     int a = 9;
@@ -9,14 +19,20 @@ int main()
     // another pointer variable
     // a is a normal variable
     p = &a;
+    // we can all the modifications of through pointer
     // pointer p is storing the address of variable a
     // if we are creating pointer variable for any variable then data
     // type must be same.
     // we can also create a pointer of a pointer.
-    int *(*p1);
-    p1 = &p;
-    printf("%d\n", *(*p1));
-    printf("%d", *p);
+    printf("%d\n", a);
+    printValue0(a); // -->call by value
+    // printValue0(&a); // -->call by address
+    printValue(p); // -->call by address
+    printf("%d\n", a);
+    // int *(*p1);
+    // p1 = &p;
+    // printf("%d\n", *(*p1));
+    // printf("%d", *p);
     printf("\n");
     return 0;
 }
